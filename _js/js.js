baa9483cb4308dc846e58858e585e4a9d1e88cd7
Farben <farbeninteractive@gmail.com>
2018-11-26 23:22:32 -0600

    Step 03 More Geometries

--- _js/js.js
+++ _js/js.js
@@ -19,15 +19,41 @@ var pointlight = new THREE.PointLight( 0xFFFFFF, 0.5 );
 scene.add( pointlight );
 
 var geometry = new THREE.CubeGeometry( 100, 100, 100 );
+// var material = new THREE.MeshBasicMaterial()
 var material = new THREE.MeshLambertMaterial({ color: 0xF3FFE2 })
 var mesh = new THREE.Mesh( geometry, material );
 mesh.position.set( 0, 0, -1000 );
 
+
+// Geometries
+var boxGeometry = new THREE.BoxGeometry( 17, 17, 17 );
+var boxMaterial = new THREE.MeshLambertMaterial({ color: 0xccff00, opacity: 0.5, transparent: true });
+var boxMesh = new THREE.Mesh( boxGeometry , boxMaterial );
+boxMesh.position.set( -10, 0, -20 );
+
+var circleGeometry = new THREE.CircleGeometry( 150 , 11 );
+var circleMaterial = new THREE.MeshLambertMaterial({ color: 0xffcc00 });
+circleMaterial.side = THREE.DoubleSide;
+var circleMesh = new THREE.Mesh( circleGeometry , circleMaterial );
+circleMesh.position.set( 200, 100, -1200 );
+
+var torusGeometry = new THREE.TorusKnotGeometry( 80 , 35 , 100 , 16 );
+var torusMaterial = new THREE.MeshLambertMaterial({ color: 0xcc00ff, opacity: 0.75, transparent: true });
+var torusMesh = new THREE.Mesh( torusGeometry, torusMaterial );
+torusMesh.position.set( 0, -100, -500 );
+
+// Add Objects on Scene
 scene.add( mesh );
+scene.add( boxMesh );
+scene.add( circleMesh );
+scene.add( torusMesh );
 
 function render() {
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.01;
+    mesh.rotation.x = mesh.rotation.y += 0.01;
+    boxMesh.rotation.x = boxMesh.rotation.y -= 0.01;
+    circleMesh.rotation.x = circleMesh.rotation.y += 0.01;
+    torusMesh.rotation.x = torusMesh.rotation.y -= 0.01;
+
     renderer.render( scene , camera );
     requestAnimationFrame( render );
 }
